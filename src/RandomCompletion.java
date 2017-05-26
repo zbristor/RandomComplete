b5c8ddf839a986101203dde3e8fb159c2bd46c6d
zbristor <zbristor@gmail.com>
2017-05-26 15:58:25 -0400

    Update RandomCompletion.java

--- src/RandomCompletion.java
+++ src/RandomCompletion.java
@@ -7,9 +7,8 @@ public class RandomCompletion
         int dienumber;
         Random rnd = new Random();
         
-        for (int i=0; i<100; i++){
         dienumber = 1 + rnd.nextInt(6);
         System.out.println("Your die roll was : " + dienumber);
-        }
+        
     }
  }
